03f16b95caa0b96ba69ce5712e090617e789e6f1
Petar Korponaić <petar.korponaic@gmail.com>
2016-04-27 14:04:15 +0200

    Added photoblog

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'perak:human2machine',
-  version: '0.0.2',
+  version: '0.0.3',
   // Brief, one-line summary of the package.
   summary: 'human2machine packaged for Meteor',
   // URL to the Git repository containing the source code for this package.
